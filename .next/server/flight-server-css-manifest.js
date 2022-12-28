94b1a5e196717e010971768ff1f043976fa58799
Beck <richardbek9608@gmail.com>
2022-12-28 22:50:16 +0500

    Metaverse

---
+++ .next/server/flight-server-css-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_CSS_MANIFEST={"__entry_css__":{"C:\\Users\\richa\\OneDrive\\Ishchi stol\\metaverse\\app\\page":["C:\\Users\\richa\\OneDrive\\Ishchi stol\\metaverse\\styles\\globals.css"]},"C:\\Users\\richa\\OneDrive\\Ishchi stol\\metaverse\\app\\layout.js":["C:\\Users\\richa\\OneDrive\\Ishchi stol\\metaverse\\styles\\globals.css"]}
---
+++ .next/static/chunks/app/layout.js
@@ -0,0 +1,28 @@
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+(self["webpackChunk_N_E"] = self["webpackChunk_N_E"] || []).push([["app\\layout"],{
+
+/***/ "./node_modules/next/dist/build/webpack/loaders/next-flight-client-entry-loader.js?modules=C%3A%5CUsers%5Cricha%5COneDrive%5CIshchi%20stol%5Cmetaverse%5Cstyles%5Cglobals.css&server=false!":
+/*!**************************************************************************************************************************************************************************************************!*\
+  !*** ./node_modules/next/dist/build/webpack/loaders/next-flight-client-entry-loader.js?modules=C%3A%5CUsers%5Cricha%5COneDrive%5CIshchi%20stol%5Cmetaverse%5Cstyles%5Cglobals.css&server=false! ***!
+  \**************************************************************************************************************************************************************************************************/
+/***/ (function(__unused_webpack_module, __unused_webpack_exports, __webpack_require__) {
+
+eval(__webpack_require__.ts("(() => __webpack_require__.e(/*! import() */ \"styles_globals_css\").then(__webpack_require__.bind(__webpack_require__, /*! ./styles/globals.css */ \"./styles/globals.css\")))//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9ub2RlX21vZHVsZXMvbmV4dC9kaXN0L2J1aWxkL3dlYnBhY2svbG9hZGVycy9uZXh0LWZsaWdodC1jbGllbnQtZW50cnktbG9hZGVyLmpzP21vZHVsZXM9QyUzQSU1Q1VzZXJzJTVDcmljaGElNUNPbmVEcml2ZSU1Q0lzaGNoaSUyMHN0b2wlNUNtZXRhdmVyc2UlNUNzdHlsZXMlNUNnbG9iYWxzLmNzcyZzZXJ2ZXI9ZmFsc2UhLmpzIiwibWFwcGluZ3MiOiJBQUFBLE9BQU8sbUtBQTJHIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vX05fRS8/ZjA5YyJdLCJzb3VyY2VzQ29udGVudCI6WyIoKCkgPT4gaW1wb3J0KC8qIHdlYnBhY2tNb2RlOiBcImxhenlcIiAqLyBcIkM6XFxcXFVzZXJzXFxcXHJpY2hhXFxcXE9uZURyaXZlXFxcXElzaGNoaSBzdG9sXFxcXG1ldGF2ZXJzZVxcXFxzdHlsZXNcXFxcZ2xvYmFscy5jc3NcIikpIl0sIm5hbWVzIjpbXSwic291cmNlUm9vdCI6IiJ9\n//# sourceURL=webpack-internal:///./node_modules/next/dist/build/webpack/loaders/next-flight-client-entry-loader.js?modules=C%3A%5CUsers%5Cricha%5COneDrive%5CIshchi%20stol%5Cmetaverse%5Cstyles%5Cglobals.css&server=false!\n"));
+
+/***/ })
+
+},
+/******/ function(__webpack_require__) { // webpackRuntimeModules
+/******/ var __webpack_exec__ = function(moduleId) { return __webpack_require__(__webpack_require__.s = moduleId); }
+/******/ __webpack_require__.O(0, ["main-app"], function() { return __webpack_exec__("./node_modules/next/dist/build/webpack/loaders/next-flight-client-entry-loader.js?modules=C%3A%5CUsers%5Cricha%5COneDrive%5CIshchi%20stol%5Cmetaverse%5Cstyles%5Cglobals.css&server=false!"); });
+/******/ var __webpack_exports__ = __webpack_require__.O();
+/******/ _N_E = __webpack_exports__;
+/******/ }
+]);
